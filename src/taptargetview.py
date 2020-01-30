4b24c6242e737f981f1d65584e21cec733e60287
shashi278 <shashiranjankv@gmail.com>
2020-01-30 17:41:09 +0530

    Added support for tracking clicks on outer/target circle -> Modified radius and font size -> Parameters to control stopping of animation based on clicks -> Removed demo code

--- src/taptargetview.py
+++ src/taptargetview.py
@@ -21,10 +21,10 @@ class TapTargetView(EventDispatcher):
     def __init__(
         self,
         widget,
-        outer_radius=dp(400),
+        outer_radius=dp(300),
         outer_circle_color= [1,0,0],
         outer_circle_alpha=.96,
-        target_radius=dp(70),
+        target_radius=dp(45),
         target_circle_color=[1,1,1],
         title_text="",
         title_text_size= dp(25),
@@ -38,6 +38,8 @@ class TapTargetView(EventDispatcher):
         cancelable=False,
         widget_position="left",
         title_position="auto",
+        stop_on_outer_touch=False,
+        stop_on_target_touch=True,
         on_end= None,
         **kwargs
         ):
@@ -45,41 +47,47 @@ class TapTargetView(EventDispatcher):
         Attributes:
         ===========
         
-        widget:             widget to add TapTargetView upon
-        outer_radius:       (optional), Radius for outer circle, defaults to dp(400)
-        outer_circle_color: (optional), Color for the outer circle, defaults to [1,0,0]
-        outer_circle_alpha: (optional), Alpha value for outer circle, defaults to .96
-        target_radius:      (optional), Radius for target circle, defaults to dp(80)
-        target_circle_color:(optional), Color for target circle, defaults to [1,1,1]
-        title_text:         (optional), Title to be shown on the view, defaults to ''
-        title_text_size:    (optional), Text size for title, defaults to sp(30)
-        title_text_color:   (optional), Text color for title, defaults to [1,1,1,1]
-        title_text_bold:    (optional), Whether title should be bold. Defaults to `True`
-        description_text:   (optional), Description to be shown below the title(Keep it short). Defaults to ''
-        description_text_size:      (optional), Text size for description text, defaults to sp(10)
-        description_text_color:     (optional), Text color for description text, defaults to [.8,.8,.8,1]
-        description_text_bold:      (optional), Whether description should be bold. Defaults to False
-        draw_shadow:        (optional), Whether to show shadow, defaults to False
-        cancelable:         (optional), Whether clicking outside the outer circle dismisses the view, defaults to False
-        widget_position:    (optional), Sets the position of the widget on the outer_circle.
-                            Can be one of "left","right","top","bottom","left_top","right_top",
-                            "left_bottom","right_bottom", and "center".
-                            Defaults to "left"
-        title_position:     (optional), Sets the position of `title_text` on the outer circle.
-                            Only works if `widget_position` is set to "center". In all other cases,
-                            it calculates the `title_position` itself.
-                            Must be set to other than "auto" when `widget_position` is set to "center".
-                            Can be one of "left","right","top","bottom","left_top","right_top",
-                            "left_bottom", and "right_bottom".
-                            Defaults to "auto"(since `widget_position` defaults to "left")
-        on_end:             (optional), Function to be called when the animation ends by clicking the button.
-                            Defaults to None
+        widget:                 widget to add TapTargetView upon
+        outer_radius:           (optional), Radius for outer circle, defaults to dp(300)
+        outer_circle_color:     (optional), Color for the outer circle, defaults to [1,0,0]
+        outer_circle_alpha:     (optional), Alpha value for outer circle, defaults to .96
+        target_radius:          (optional), Radius for target circle, defaults to dp(45)
+        target_circle_color:    (optional), Color for target circle, defaults to [1,1,1]
+        title_text:             (optional), Title to be shown on the view, defaults to ''
+        title_text_size:        (optional), Text size for title, defaults to dp(25)
+        title_text_color:       (optional), Text color for title, defaults to [1,1,1,1]
+        title_text_bold:        (optional), Whether title should be bold. Defaults to `True`
+        description_text:       (optional), Description to be shown below the title(Keep it short)
+                                Defaults to ''
+        description_text_size:  (optional), Text size for description text, defaults to dp(20)
+        description_text_color: (optional), Text color for description text, defaults to [.9,.9,.9,1]
+        description_text_bold:  (optional), Whether description should be bold. Defaults to False
+        draw_shadow:            (optional), Whether to show shadow, defaults to False
+        cancelable:             (optional), Whether clicking outside the outer circle dismisses the view
+                                Defaults to False
+        widget_position:        (optional), Sets the position of the widget on the outer_circle.
+                                Can be one of "left","right","top","bottom","left_top","right_top",
+                                "left_bottom","right_bottom", and "center".
+                                Defaults to "left"
+        title_position:         (optional), Sets the position of `title_text` on the outer circle.
+                                Only works if `widget_position` is set to "center". In all other cases,
+                                it calculates the `title_position` itself.
+                                Must be set to other than "auto" when `widget_position` is set to "center".
+                                Can be one of "left","right","top","bottom","left_top","right_top",
+                                "left_bottom", and "right_bottom".
+                                Defaults to "auto"(since `widget_position` defaults to "left")
+        stop_on_outer_touch:    (optional), whether clicking on outer circle stops the animation
+                                Defaults to False
+        stop_on_target_touch:   (optional), whether clicking on target circle should stop the animation
+                                Defaults to True
+        on_end:                 (optional), Function to be called when the animation ends by clicking the button.
+                                Defaults to None
         """
         self.widget= widget
-        self.outer_radius= outer_radius
+        self.outer_radius= 2*outer_radius
         self.outer_circle_color= outer_circle_color
         self.outer_circle_alpha= outer_circle_alpha
-        self.target_radius= target_radius
+        self.target_radius= 2*target_radius
         self.target_circle_color= target_circle_color
         self.title_text_size= title_text_size
         self.title_text_color= title_text_color
@@ -91,12 +99,11 @@ class TapTargetView(EventDispatcher):
         self.cancelable= cancelable
         self.widget_position= widget_position
         self.title_position= title_position
+        self.stop_on_outer_touch= stop_on_outer_touch
+        self.stop_on_target_touch= stop_on_target_touch
         self.on_end= on_end
 
         self.ripple_max_dist=dp(70)
-
-        #just to track if it's already been ended or not
-        self._count=0
 
         self.title_text= CoreLabel(
             text=title_text,
@@ -113,15 +120,23 @@ class TapTargetView(EventDispatcher):
         self.description_text= self.description_text.texture
 
         super(TapTargetView,self).__init__(**kwargs)
-    
+        self.register_event_type("on_outer_touch")
+        self.register_event_type("on_target_touch")
+
     def _initialize(self):
         setattr(self.widget, "outer_radius", 0)
         setattr(self.widget, "target_radius", 0)
         setattr(self.widget, "target_ripple_radius", 0)
         setattr(self.widget, "target_ripple_alpha",0)
+
+        # Note:
+        # bind some function on widget event when this function is called instead of
+        # when the class itself is initialized to prevent all widgets of all instances
+        # to get bind at once and start messing up
+        self.widget.bind(on_touch_down=self._some_func)
     
     def draw_canvas(self):
-        _pos= self.ttv_pos()
+        _pos= self._ttv_pos()
 
         self.widget.canvas.before.clear()
         with self.widget.canvas.before:
@@ -184,13 +199,17 @@ class TapTargetView(EventDispatcher):
         )
         anim.bind(on_complete= self._after_stop)
         anim.start(self.widget)
-        self._count+=1
         
     def _after_stop(self, *args):
         self.widget.canvas.before.clear()
         args[0].stop_all(self.widget)
-        if self._count==1 and self.on_end is not None:
+        if self.on_end:
             self.on_end()
+
+        # Note:
+        # Don't forget to unbind the function or it'll mess
+        # up with other next bindings
+        self.widget.unbind(on_touch_down=self._some_func)
     
     def start(self):
         self._initialize()
@@ -226,13 +245,60 @@ class TapTargetView(EventDispatcher):
         setattr(self.widget, "target_ripple_alpha",1)
         self._animate_ripple()
     
-    def on_target_click(self):
-        pass
+    def on_target_touch(self):
+        #print("Clicked on target circle")
+        if self.stop_on_target_touch:
+            self.stop()
     
-    def on_outer_click(self):
-        pass
+    def on_outer_touch(self):
+        #print("Clicked on outer circle")
+        if self.stop_on_outer_touch:
+            self.stop()
+    
+    def _some_func(self, wid, touch):
+        """
+        This function decides which one to dispatch
+        based on the touch position
+        """
+        if self._check_pos_outer(touch.pos) and not self._check_pos_target(touch.pos):
+            self.dispatch("on_outer_touch")
 
-    def ttv_pos(self):
+        if self._check_pos_target(touch.pos):
+            self.dispatch("on_target_touch")
+    
+    def _check_pos_outer(self, pos):
+        """
+        Checks if a given `pos` coordinate is within the
+        `outer_radius`
+        """
+        cx= self.circ_pos[0]+self.outer_radius/2
+        cy= self.circ_pos[1]+self.outer_radius/2
+        r= self.outer_radius/2
+        h,k= pos
+
+        lhs= (cx-h)**2+(cy-k)**2
+        rhs= r**2
+        if lhs<=rhs:
+            return True
+        return False
+            
+    def _check_pos_target(self,pos):
+        """
+        Checks if a given `pos` coordinate is within the
+        `target_radius`
+        """
+        cx= self.widget.pos[0]+self.widget.width/2
+        cy= self.widget.pos[1]+self.widget.height/2
+        r= self.target_radius/2
+        h,k= pos
+
+        lhs= (cx-h)**2+(cy-k)**2
+        rhs= r**2
+        if lhs<=rhs:
+            return True
+        return False
+
+    def _ttv_pos(self):
         """
         Calculates the `pos` value for outer circle and text
         based on the position provided
@@ -312,166 +378,6 @@ class TapTargetView(EventDispatcher):
             else:
                 raise ValueError("'{}' is not a valid value for title_position".format(self.title_position))
             
-
+        self.circ_pos= circ_pos
         return circ_pos,title_pos
 
-
-
-if __name__ == "__main__":
-    from kivy.app import App
-    from kivy.lang import Builder
-
-    from kivymd.uix.button import MDIconButton
-    from kivymd.theming import ThemeManager
-
-    
-    example_kv="""
-BoxLayout:
-    orientation:"vertical"
-
-    BoxLayout:
-        size_hint_y: None
-        height: dp(60)
-        canvas:
-            Color:
-                rgba: app.theme_cls.primary_color
-            Rectangle:
-                pos: self.pos
-                size: self.size
-        
-        AnchorLayout:
-            anchor_x:"left"
-            MDIconButton:
-                id: menu_btn
-                md_bg_color: app.theme_cls.primary_color
-                #theme_text_color:"Custom"
-                #text_color:0,0,0,1
-                icon: "menu"
-                opposite_colors: True
-
-        
-        BoxLayout:
-            AnchorLayout:
-            BoxLayout:
-                AnchorLayout:
-                    MDIconButton:
-                        id: search_btn
-                        md_bg_color: 0,0,0,0
-                        theme_text_color:"Custom"
-                        text_color:1,1,1,1
-                        icon: "magnify"
-                        opposite_colors: True
-
-                AnchorLayout:
-                    MDIconButton:
-                        id: info_btn
-                        md_bg_color: 0,0,0,0
-                        theme_text_color:"Custom"
-                        text_color:1,1,1,1
-                        icon: "information-outline"
-                        opposite_colors: True
-
-    
-    BoxLayout:
-        orientation:"vertical"
-        Label:
-            id: lbl
-            text: ""
-            color: .8,.8,.8,1
-            font_size: sp(50)
-
-        BoxLayout:
-            AnchorLayout:
-                size_hint_x:.3
-                MDFloatingActionButton:
-                    id: add_btn
-                    icon: 'plus'
-            AnchorLayout:
-
-
-"""
-
-    class TapTargetViewDemo(App):
-        theme_cls= ThemeManager()
-
-        def build(self):
-            x= Builder.load_string(example_kv)
-            menu_btn= x.ids.menu_btn
-            search_btn= x.ids.search_btn
-            info_btn= x.ids.info_btn
-            add_btn= x.ids.add_btn
-
-            self.lbl= x.ids.lbl
-            self.final_text="Congrats! You're \n educated now!!"
-
-            ttv4= TapTargetView(
-                add_btn,
-                outer_radius=dp(450),
-                outer_circle_color= self.theme_cls.primary_color[:-1],
-                outer_circle_alpha= .9,
-                target_radius=dp(90),
-                target_circle_color= [1,1,1],
-                title_text= "This is an add button",
-                description_text="Click here to add a new data",
-                widget_position="left_bottom",
-
-                on_end=self.set_text
-            )
-
-            ttv3= TapTargetView(
-                info_btn,
-                outer_radius=dp(650),
-                outer_circle_color= self.theme_cls.primary_color[:-1],
-                outer_circle_alpha= .8,
-                target_radius=dp(90),
-                target_circle_color=[255/255, 34/255, 212/255],
-                title_text= "This is the info button",
-                description_text="No information available yet!",
-                widget_position="center",
-                title_position="left_bottom",
-
-                on_end=ttv4.start
-            )
-
-            ttv2= TapTargetView(
-                search_btn,
-                outer_radius=dp(600),
-                outer_circle_color= [155/255, 89/255, 182/255],
-                outer_circle_alpha= .95,
-                target_radius=dp(90),
-                target_circle_color=[.2,.2,.2],
-                title_text= "This is the search button",
-                description_text="It won't search anything for now.",
-                widget_position="center",
-                title_position="left_bottom",
-
-                on_end=ttv3.start
-            )
-
-            ttv1= TapTargetView(
-                menu_btn,
-                outer_radius=dp(600),
-                outer_circle_color= self.theme_cls.primary_color[:-1],
-                outer_circle_alpha= .85,
-                target_radius=(100),
-                title_text= "Menu Button",
-                description_text="Opens up the drawer",
-                widget_position="center",
-                title_position="right_bottom",
-
-                on_end= ttv2.start
-            )
-
-            ttv1.start()
-
-            menu_btn.bind(on_release= ttv1.stop)
-            search_btn.bind(on_release= ttv2.stop)
-            info_btn.bind(on_release= ttv3.stop)
-            add_btn.bind(on_release= ttv4.stop)
-
-            return x
-        
-        def set_text(self):
-            self.lbl.text= self.final_text
-    
-    TapTargetViewDemo().run()
